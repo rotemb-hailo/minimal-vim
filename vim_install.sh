dcca5f01f2871fbd6cc718becbfc1977ac7292c3
Rotem Bar <rotemb@hailo.ai>
2023-08-21 10:15:17 +0300

    add install automaticlly

--- vim_install.sh
+++ vim_install.sh
@@ -7,4 +7,6 @@ sudo apt-get install -y curl fzf
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
+rm -f ~/.vimrc
+cp .vimrc ~/.vimrc
+vim ~/.vimrc +PlugInstall + q!
