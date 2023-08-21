e491b56f708b2b7a112a4f9cb2d5e8ad9b7eb339
Rotem Bar <rotemb@hailo.ai>
2023-08-21 14:30:07 +0300

    add commentsgs

--- vim_install.sh
+++ vim_install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
+# vim-gtk3 needed for clipboard - https://vi.stackexchange.com/a/31142
 sudo apt-get update
-sudo apt-get install -y curl fzf
+sudo apt-get install -y curl fzf vim-gtk3
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
